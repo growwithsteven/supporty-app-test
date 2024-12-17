894be62a4ccb6f7c24cb807eb529710f1ec33f91
greatSumini <greatSumini@gmail.com>
2024-12-17 22:19:17 +0900

    chore: js -> ts

--- apps/web/app/api/slack/register/route.ts
+++ apps/web/app/api/slack/register/route.ts
@@ -1,93 +1,93 @@
-import * as slackApi from '@/lib/slack-api'
+import * as slackApi from "@/lib/slack-api";
 
-import { NextResponse } from 'next/server'
-import { createSupabaseWithServiceRole } from '@/lib/supabase'
-import { createToken } from '@/lib/token'
+import { NextResponse } from "next/server";
+import { createSupabaseWithServiceRole } from "@/lib/supabase";
+import { createToken } from "@/lib/token";
 
-export async function POST(req) {
-  const { code } = await req.json()
-  const supabase = createSupabaseWithServiceRole()
+export async function POST(req: Request) {
+  const { code } = await req.json();
+  const supabase = createSupabaseWithServiceRole();
 
   // Get access token
-  const { data: oauthData } = await slackApi.oauthAccess(code)
+  const { data: oauthData } = await slackApi.oauthAccess(code);
 
   if (oauthData.error) {
-    return NextResponse.json({ error: oauthData.error }, { status: 401 })
+    return NextResponse.json({ error: oauthData.error }, { status: 401 });
   }
 
   // Get project
   let { data: project } = await supabase
-    .from('projects')
-    .select('*')
-    .eq('team_id', oauthData.team.id)
-    .single()
+    .from("projects")
+    .select("*")
+    .eq("team_id", oauthData.team.id)
+    .single();
 
   if (project) {
     // If project exists, update it
     const { data: updatedProject } = await supabase
-      .from('projects')
+      .from("projects")
       .update({
         access_token: oauthData.access_token,
         team_name: oauthData.team.name,
         channel_id: oauthData.incoming_webhook.channel_id,
       })
-      .eq('team_id', oauthData.team.id)
-      .select('*')
-      .single()
+      .eq("team_id", oauthData.team.id)
+      .select("*")
+      .single();
 
-    project = updatedProject
+    project = updatedProject;
   } else {
     // If project does not exist, create it
     const { data: newProject } = await supabase
-      .from('projects')
+      .from("projects")
       .insert({
         access_token: oauthData.access_token,
         team_id: oauthData.team.id,
         team_name: oauthData.team.name,
         channel_id: oauthData.incoming_webhook.channel_id,
       })
-      .select('*')
-      .single()
+      .select("*")
+      .single();
 
     // Create project details
-    await supabase.from('project_details').insert({
+    await supabase.from("project_details").insert({
       project_uuid: newProject.uuid,
       settings: {
-        welcomeMessage: 'Welcome to the chat!',
+        welcomeMessage: "Welcome to the chat!",
       },
-    })
+    });
 
-    project = newProject
+    project = newProject;
   }
 
   // Join channel
   const { data: joinChannelData } = await slackApi.joinChannel({
     accessToken: project.access_token,
     channel: project.channel_id,
-  })
+  });
 
-  const welcomeMessage = `Hello, ${project.team_name}!\nMessages from supporty will appear in this channel!`
+  const welcomeMessage = `Hello, ${project.team_name}!\nMessages from supporty will appear in this channel!`;
   if (joinChannelData.error) {
     // Send error message to channel
     await slackApi.postMessageToWebhook({
       webhookUrl: oauthData.incoming_webhook.url,
       text: `${welcomeMessage}\nSupporty can not join private channel. Please invite supporty bot to this channel.`,
-    })
+    });
   } else {
     // Send welcome message to channel
     await slackApi.postMessageToWebhook({
       webhookUrl: oauthData.incoming_webhook.url,
       text: welcomeMessage,
-    })
+    });
   }
 
   // Create token
   const token = await createToken({
     sub: project.uuid,
-  })
+  });
 
   return NextResponse.json({
     project: { team: { name: project.team_name }, uuid: project.uuid },
     token,
-  })
+  });
 }
